65d923eccb2338a9da3cbf8b5bcdcee66944e429
Habibullo <57355966+Habibulo@users.noreply.github.com>
2024-05-16 03:24:32 +0900

    server.js va shopping.ejs file ozgardi

---
+++ server.js
@@ -0,0 +1,66 @@
+// 18-19 Darslar
+console.log('web serverni boshlash')
+// buyerdan biz expressni ornatamiz [npm install express --save]
+// va instance yasaymiz
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const { default: test } = require('node:test');
+// 4 bosqichli server
+
+// 1 Kirish uchun Codes
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))  // html form dan birorbir narsani post qilsak qabul qilib oladi [traditional formatda]
+
+// 2 Session Codes
+
+// 3 Views Codes
+// Express uchun BSSR yonalishi = Backend Server Side Rending
+
+app.set('views', 'views')
+app.set('view engine', 'ejs')
+
+// buyerda backendni ichida frontend uchun html yasaymiz [ traditional yonalishida ]
+
+// 4 Route Codes
+// app.get('/hello', function(req, res){
+//     res.end('<h1>Hello World by Daniel</h1>')
+// })
+// app.get('/gifts', function(req, res){
+//     res.end('<h1>Siz endi Sovgalar bolimidasiz</h1>')
+// })
+// app.get('/', function(req, res){
+//     res.render("shopping")
+// }) 
+app.get('/', function(req, res){
+    res.render("shopping")
+}) //malumotni databasadan olib keladi
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: 'success' });
+}); //malumotni databasada mutatsiyaga uchratadi: {create, update, delete}
+
+const server = http.createServer(app)
+let PORT = 3000
+server.listen(PORT, function() {
+    console.log(`The server is successfully is working on port: ${PORT}`)
+})
+/* git ga commit uchun 
+    > npm i git --save
+    > npm init
+    > git status [Untracked files = .gitignore, package.json.....]
+    > git add .
+    > git commit -m "BRR: Build Express Web Server"
+    > git log --oneline
+
+*/
+// 18-19 dars tugashi*/
+
+
+// 20-21 Darslar
+
+// 20-21 Darslar tugashi*/
