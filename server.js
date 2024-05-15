143bf8667e43a9dc7e1fd6582306706a292ac294
Habibullo <57355966+Habibulo@users.noreply.github.com>
2024-05-16 03:13:07 +0900

    feat: serverjs shopping ejs ochirilib qaytadan yuklandi

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // 18-19 Darslar
 console.log('web serverni boshlash')
-
 // buyerdan biz expressni ornatamiz [npm install express --save]
-
+// va instance yasaymiz
 const express = require('express');
 const app = express();
-const http = require('http')
+const http = require('http');
+const bodyParser = require('body-parser');
+const { default: test } = require('node:test');
 // 4 bosqichli server
 
 // 1 Kirish uchun Codes
 app.use(express.static('public'))
+app.use(bodyParser.json());
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))  // html form dan birorbir narsani post qilsak qabul qilib oladi [traditional formatda]
 
@@ -23,25 +25,30 @@ app.set('view engine', 'ejs')
 
 // buyerda backendni ichida frontend uchun html yasaymiz [ traditional yonalishida ]
 
-
 // 4 Route Codes
-app.get('/hello', function(req, res){
-    res.end('<h1>Hello World by Daniel</h1>')
-})
-app.get('/gifts', function(req, res){
-    res.end('<h1>Siz endi Sovgalar bolimidasiz</h1>')
-})
+// app.get('/hello', function(req, res){
+//     res.end('<h1>Hello World by Daniel</h1>')
+// })
+// app.get('/gifts', function(req, res){
+//     res.end('<h1>Siz endi Sovgalar bolimidasiz</h1>')
+// })
+// app.get('/', function(req, res){
+//     res.render("shopping")
+// }) 
 app.get('/', function(req, res){
     res.render("shopping")
-})
+}) //malumotni databasadan olib keladi
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({ test: 'success' });
+}); //malumotni databasada mutatsiyaga uchratadi: {create, update, delete}
 
 const server = http.createServer(app)
 let PORT = 3000
 server.listen(PORT, function() {
     console.log(`The server is successfully is working on port: ${PORT}`)
 })
-
-
 /* git ga commit uchun 
     > npm i git --save
     > npm init
@@ -49,7 +56,7 @@ server.listen(PORT, function() {
     > git add .
     > git commit -m "BRR: Build Express Web Server"
     > git log --oneline
-    
+
 */
 // 18-19 dars tugashi*/
 
