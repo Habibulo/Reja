c13e38b68e15703bc5d035389bccb9883d2926f0
Habibullo <57355966+Habibulo@users.noreply.github.com>
2024-05-22 01:45:53 +0900

    fix: reja file larni ozgartirdim! feat: portfolioni olib tashlab endi darsliklar va tasklarni qoyib boraman

--- train.js
+++ train.js
@@ -1,1 +1,15 @@
-// new comment
+// Here 21th & 22th Tasks start!
+// 2024-05-20
+// MIT 14 TASK A
+// Harf sifatida kiritilgan birinchi parametr, 
+// kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+// Funktsiya tuzing
+
+// Masalan: countLetter("e", "engineer")
+// 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+// 3 sonini qaytaradi
+
+
+// ===================================================================================
+// const task_a = require("./tasks/task_a/task.js")
+const lesson_21_22 = require("./Lessons/lesson_22_23/train.js")
