d500b5e142d1b1a1842d45547b775c0bb42fd14e
Habibullo <57355966+Habibulo@users.noreply.github.com>
2024-05-22 03:35:30 +0900

    feat: with different approaches

--- tasks/task_a/approach_2.js
+++ tasks/task_a/approach_2.js
@@ -7,3 +7,25 @@
 // 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
 // 3 sonini qaytaradi
 
+
+const character = "e";
+const sentence = "engineer";
+
+const countLetter = (character, sentence) => {
+  let count = 0;
+
+  // Split the sentence into an array of characters
+  const characters = sentence.split("");
+
+  for (let i = 0; i < characters.length; i++) {
+    if (characters[i] === character) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+const result = countLetter(character, sentence);
+console.log(`'${character}' letter is repeated in '${sentence}' sentence '${result}' times`);  // Output: 'e' letter is repeated in 'engineer' sentence '3' times
+
