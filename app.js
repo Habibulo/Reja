dde228cf8a5e256a5dfbcdf71c3fa8e993e42930
Habibullo <57355966+Habibulo@users.noreply.github.com>
2024-05-25 00:16:45 +0900

    fix: added few elements

--- app.js
+++ app.js
@@ -1,77 +1,101 @@
-console.log('web serverni boshlash')
-const ejs = require('ejs');
 const express = require('express');
-const app = express();
+const ejs = require('ejs');
 const fs = require('fs');
-const { ObjectId } = require('mongodb');  // Import ObjectId from mongodb
+const { ObjectId } = require('mongodb');
+const app = express();
 
-let user 
+let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
     if (err) {
-        console.log("ERROR", err)
+        console.log("ERROR", err);
     } else {
-        user = JSON.parse(data)
+        user = JSON.parse(data);
     }
-})
+});
 
 // 1.MongoDB Connect
 const db = require("./server").db();
 
-// 1 Kirish uchun Codes
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))  // html form dan birorbir narsani post qilsak qabul qilib oladi [traditional formatda]
+if (!db) {
+    console.log("Database connection failed!");
+}
 
-// 2 Session Codes
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// 3 Views Codes
-app.set('views', 'views')
-app.set('view engine', 'ejs')
+// Views
+app.set('views', 'views');
+app.set('view engine', 'ejs');
 
-// 4 Route Codes
+// Routes
 app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
     const new_plan = req.body.plan;
+    console.log("Plan to be added:", new_plan);
     db.collection("reja").insertOne({ plan: new_plan }, (err, data) => {
-      if (err) {
-        console.log("error on create-item request: ", err.message);
-        res.end("(app.post/create-item) something went wrong!");
-      } else {
-        res.end("(app.post/create-item) added successfully!");
-      }
+        if (err) {
+            console.log("error on create-item request: ", err.message);
+            res.status(500).end("(app.post/create-item) something went wrong!");
+        } else {
+            console.log("Item added successfully:", data);
+            res.status(200).end("(app.post/create-item) added successfully!");
+        }
     });
 });
 
-app.get("/", function (req, res) {
-	console.log("user entered /")
-	db.collection("reja")
-		.find()
-		.toArray((err, data) => {
-			if (err) {
-				console.log("Collection Error: ", err.message);
-				res.end("(app.get/) something went wrong");
-			} else {
-				console.log("Collection Data: ", data);
-				res.render("reja", { items: data });
-			}
-		});
+app.get("/", (req, res) => {
+    console.log("user entered /");
+    db.collection("reja").find().toArray((err, data) => {
+        if (err) {
+            console.log("Collection Error: ", err.message);
+            res.status(500).end("(app.get/) something went wrong");
+        } else {
+            console.log("Collection Data: ", data);
+            res.render("reja", { items: data });
+        }
+    });
 });
 
-app.delete("/delete-item", function (req, res){
-    console.log("user deleted /")
+app.delete("/delete-item", (req, res) => {
+    console.log("user deleted /");
     const itemId = req.body.id;
+    console.log("Item ID to delete:", itemId);
     db.collection("reja").deleteOne({ _id: new ObjectId(itemId) }, (err, result) => {
         if (err) {
             console.log("error on delete-item request: ", err.message);
-            res.end("(app.delete/delete-item) something went wrong!");
+            res.status(500).end("(app.delete/delete-item) something went wrong!");
         } else {
-            res.end("(app.delete/delete-item) deleted successfully!");
+            console.log("Item deleted successfully:", result);
+            res.status(200).end("(app.delete/delete-item) deleted successfully!");
+        }
+    });
+});
+
+app.delete("/delete-all-items", (req, res) => {
+    console.log("user requested to delete all items");
+    db.collection("reja").deleteMany({}, (err, result) => {
+        if (err) {
+            console.log("error on delete-all-items request: ", err.message);
+            res.status(500).end("(app.delete/delete-all-items) something went wrong!");
+        } else {
+            console.log("All items deleted successfully:", result);
+            res.status(200).end("(app.delete/delete-all-items) all items deleted successfully!");
         }
     });
 });
 
 app.get('/author', (req, res) => {
     res.render("author", { user: user });
-})
+});
 
 module.exports = app;
+
+/* MongodDB
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT => DATASET
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT => DATASET
+
+
+// ObjectId lar ni => dataset deb ataymiz
+*/
