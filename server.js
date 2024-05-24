cf96499b0032ccb755d882105d76ae481a2e81c3
Habibullo <57355966+Habibulo@users.noreply.github.com>
2024-05-24 20:52:58 +0900

    fix: require db() function oqiydi

--- server.js
+++ server.js
@@ -1,71 +1,30 @@
-//  Darslar
-// 23-24-25 
-// CRUD OPERATIONS 
+const http = require('http');
 
-console.log('web serverni boshlash')
-const ejs = require('ejs');
-const express = require('express');
-const app = express();
-const fs = require('fs');
-const { json } = require('stream/consumers');
+const mongodb= require('mongodb');  // Correctly import MongoClient from the mongodb package
 
-let user 
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if (err) {
-        console.log("ERROR", err)
-    } else {
-        user = JSON.parse(data)
+let db;
+const connectionString = 
+    "mongodb+srv://Khalimjonov:viartmnM9184j4XS@train.7tbb2na.mongodb.net/Reja"
+
+// const connectionString =
+//   "mongodb+srv://admin:admin@cluster0.vgohadt.mongodb.net/reja?retryWrites=true&w=majority&appName=Cluster0";
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+ }, (err, client) => {
+    if(err) {
+        console.log('Error on connection MongoDB:', err)
+    }
+    else {
+        console.log("MongoDB successfully connected!")
+        // console.log(client).
+        module.exports = client;
+        const app = require("./app")
+        const server = http.createServer(app)
+        let PORT = 3000
+        server.listen(PORT, function() {
+            console.log(`The server is successfully is working on port: ${PORT} http://localhost:${PORT}`)
+            console.log(`Portfolio is working on port: ${PORT} http://localhost:${PORT}/author`)
+        })
     }
 })
-
-// 1.MongoDB Connect
-const db = require("./server").db();
-
-// 1 Kirish uchun Codes
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))  // html form dan birorbir narsani post qilsak qabul qilib oladi [traditional formatda]
-
-// 2 Session Codes
-
-
-// 3 Views Codes
-app.set('views', 'views')
-app.set('view engine', 'ejs')
-
-
-// 4 Route Codes
-app.post("/create-item", (req, res) => {
-    console.log("user entered /create-item");
-    const new_plan = req.body.plan;
-    db.collection("reja").insertOne({ plan: new_plan }, (err, data) => {
-      if (err) {
-        console.log("error on create-item request: ", err.message);
-        res.end("(app.post/create-item) something went wrong!");
-      } else {
-        res.end("(app.post/create-item) added successfully!");
-      }
-    });
-  });
-//malumotni databasada mutatsiyaga uchratadi: {create, update, delete}
-app.get("/", function (req, res) {
-    console.log("user entered /")
-    db.collection("plan")
-      .find()
-      .toArray((err, data) => {
-        if (err) {
-          console.log("Collection Error: ", err.message);
-          res.end("(app.get/) something went wrong");
-        } else {
-          console.log("Collection Data: ", data);
-          res.render("reja", { items: data});
-        }
-      });
-  });
-app.get('/author', (req, res) => {
-    res.render("author", {user: user})
-})
-
-
-
-module.exports = app;
